3228830e54a10279d494e3f3b0fef7376fda25c5
Janosch Kindl <janoschkindl+srht@gmail.com>
2024-11-08 14:00:52 +0100

    Adapted main

--- src/Main.hs
+++ src/Main.hs
@@ -7,22 +7,24 @@ import Data.List(isSuffixOf)
 import qualified Golike
 import qualified Clike
 import Parser
+import Drucker(toText)
 
 
 main = do
     args <- getArgs
     case args of
-        [path] -> runDependingOnEnding path
+        [path] -> compileDependingOnEnding path
         _ -> putStrLn "Run with a input path like this `cabal run exes -- examples/example.h`"
 
-run parser inputPath outputPath = do
+compile parser inputPath outputPath = do
     content <- Text.readFile inputPath
     parsed <- either fail return (parser content)
     transformed <- runTransformations parsed
-    Text.writeFile outputPath (toQbe transformed)
+    Text.writeFile outputPath (toText (toQbe transformed))
+    Text.writeFile "out/Test.cs" (toText (toCs parsed))
     putStrLn ("Written to " ++ outputPath)
 
-runDependingOnEnding path =
+compileDependingOnEnding path =
     if isSuffixOf ".guo" path
-        then run (parse Golike.statements) path "out/golike.ssa"
-        else run (parse Clike.statements) path "out/clike.ssa"
+        then compile (parse Golike.statements) path "out/golike.ssa"
+        else compile (parse Clike.statements) path "out/clike.ssa"
